b3e5252c7be3ba8f9e539fa144e66409d3358d1a
izida <myizida@yandex.ru>
2024-08-06 10:40:33 +0300

    chapter1 & little bit chapter2

---
+++ dags/chapter02/download_rocket_launches.py
@@ -0,0 +1,59 @@
+import json
+import os
+import pathlib
+
+import airflow.utils.dates
+import requests
+import requests.exceptions as requests_exceptions
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+
+dag = DAG(
+    dag_id="download_rocket_launches",
+    description="Download rocket pictures od recently launched rockets",
+    start_date=airflow.utils.dates.days_ago(14),
+    schedule_interval="@daily"
+)
+
+download_launches = BashOperator(
+    task_id="download_launches",
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'", # noqas
+    dag=dag
+)
+
+
+def _get_pictures():
+    os.makedirs("/tmp/launches", exist_ok=True)
+
+    with open("/tmp/launches.json", "r") as f:
+        launches = json.load(f)
+        image_urls = [launch.get["image"] for launch in launches["results"]]
+        for image_url in image_urls:
+            try:
+                response = requests.get(image_url)
+                image_filename = image_url.split("/")[-1]
+                target_file = f"/tmp/images/{image_filename}"
+                with open(target_file, "wb") as f:
+                    f.write(response.content)
+                print(f"Downloaded {image_url} to {target_file}")
+            except requests_exceptions.MissingSchema:
+                print(f"{image_url} appears to be an invalid URL.")
+            except requests_exceptions.ConnectionError:
+                print(f"Could not connect to {image_url}.")
+
+
+get_picture = PythonOperator(
+    task_id="get_pictures",
+    python_callable=_get_pictures,
+    dag=dag
+)
+
+notify = BashOperator(
+    task_id="notify",
+    bash_command="echo 'There are now $(ls /tmp/images/ | wc -l) images.'",
+    dag=dag
+)
+
+download_launches >> get_picture >> notify
