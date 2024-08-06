29ad4afc8b2934b4c17df76283f70acef97b7d05
izida <myizida@yandex.ru>
2024-08-06 15:09:06 +0300

    add rocket launches, dir goods for some goods practise

--- dags/chapter02/download_rocket_launches.py
+++ dags/chapter02/download_rocket_launches.py
@@ -1,6 +1,5 @@
 import json
 import os
-import pathlib
 
 import airflow.utils.dates
 import requests
@@ -8,7 +7,13 @@ import requests.exceptions as requests_exceptions
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
+import logging
 
+from custom_errors import NoDataError
+
+
+# get the airflow.task logger
+task_logger = logging.getLogger('airflow.task')
 
 dag = DAG(
     dag_id="download_rocket_launches",
@@ -19,7 +24,7 @@ dag = DAG(
 
 download_launches = BashOperator(
     task_id="download_launches",
-    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'", # noqas
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'", # noqa
     dag=dag
 )
 
@@ -29,19 +34,29 @@ def _get_pictures():
 
     with open("/tmp/launches.json", "r") as f:
         launches = json.load(f)
-        image_urls = [launch.get["image"] for launch in launches["results"]]
+
+        if len(launches["results"]) > 0:
+            task_logger.info("Data load")
+        else:
+            raise NoDataError("No data found")
+
+        image_urls = [launch.get("image") for launch in launches["results"]]
+
         for image_url in image_urls:
             try:
                 response = requests.get(image_url)
                 image_filename = image_url.split("/")[-1]
                 target_file = f"/tmp/images/{image_filename}"
+
                 with open(target_file, "wb") as f:
                     f.write(response.content)
-                print(f"Downloaded {image_url} to {target_file}")
+                task_logger.info(f"Downloaded {image_url} to {target_file}")
+
             except requests_exceptions.MissingSchema:
-                print(f"{image_url} appears to be an invalid URL.")
+                task_logger.error(f"{image_url} appears to be an invalid URL.")
+
             except requests_exceptions.ConnectionError:
-                print(f"Could not connect to {image_url}.")
+                task_logger.error(f"Could not connect to {image_url}.")
 
 
 get_picture = PythonOperator(
---
+++ dags/goods/logger_example.py
@@ -0,0 +1,54 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+
+# import the logging module
+import logging
+
+# get the airflow.task logger
+task_logger = logging.getLogger('airflow.task')
+
+
+@task.python
+def extract():
+
+    # with default airflow logging settings, DEBUG logs are ignored
+    task_logger.debug('This log is at the level of DEBUG')
+
+    # each of these lines produces a log statement
+    print('This log is created via a print statement')
+    task_logger.info('This log is informational')
+    task_logger.warning('This log is a warning')
+    task_logger.error('This log shows an error!')
+    task_logger.critical('This log shows a critical error!')
+
+    data = {'a': 19, 'b': 23, 'c': 42}
+
+    # Using the Task flow API to push to XCom by returning a value
+    return data
+
+
+# logs outside of tasks will not be processed
+task_logger.warning('This log will not show up!')
+
+
+with DAG(dag_id='more_logs_dag',
+         start_date=datetime(2022, 6, 5),
+         schedule_interval='@daily',
+         dagrun_timeout=timedelta(minutes=10),
+         catchup=False) as dag:
+
+    # command to create a file and write the data from the extract task into it
+    # these commands use Jinja templating within {{}}
+    commands = """
+        touch /usr/local/airflow/{{ds}}.txt
+        echo {{ti.xcom_pull(task_ids='extract')}} > /usr/local/airflow/{{ds}}.txt
+        """
+
+    write_to_file = BashOperator(task_id='write_to_file', bash_command=commands) # noqa
+
+    # logs outside of tasks will not be processed
+    task_logger.warning('This log will not show up!')
+
+    extract() >> write_to_file
